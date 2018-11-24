c14f8b380a3fcfb16b6ecdba7e4df65d0998ac30
Jonathan Chiam <jczjchiam@gmail.com>
2018-11-24 23:44:57 +0800

    Update photo 2 and refactor some code

--- src/SaveTheDate.jsx
+++ src/SaveTheDate.jsx
@@ -2,26 +2,38 @@ import React, { Component } from 'react';
 import Slideshow from 'react-slidez';
 
 export default class SaveTheDate extends Component {
+  static renderSlideshow() {
+    return (
+      <Slideshow
+        showIndex={false}
+        showArrows={false}
+        autoplay
+        enableKeyboard={false}
+        useDotIndex
+        slideInterval={8000}
+        defaultIndex={0}
+        slides={[require('images/1.png'), require('images/2.png')]}
+        effect={'fade'}
+      />
+    );
+  }
+
+  static renderDetails() {
+    return (
+      <div className="details">
+        <span>Rebecca + Jonathan</span>
+        <span>April 6, 2019</span>
+        <button>RSVP Here</button>
+      </div>
+    );
+  }
+
   render() {
     return (
       <>
         <h1>Save The Date</h1>
-        <Slideshow
-          showIndex={false}
-          showArrows={false}
-          autoplay
-          enableKeyboard={false}
-          useDotIndex
-          slideInterval={8000}
-          defaultIndex={0}
-          slides={[require('images/1.png'), require('images/2.jpg')]}
-          effect={'fade'}
-        />
-        <div className="details">
-          <span>Rebecca + Jonathan</span>
-          <span>April 6, 2019</span>
-          <button>RSVP Here</button>
-        </div>
+        {SaveTheDate.renderSlideshow()}
+        {SaveTheDate.renderDetails()}
       </>
     );
   }
