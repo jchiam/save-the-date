f0b61a9ea702306716db896f96b2fe42b9cafd65
Jonathan Chiam <jczjchiam@gmail.com>
2018-11-25 01:21:39 +0800

    Add transformations to photos

--- src/SaveTheDate.jsx
+++ src/SaveTheDate.jsx
@@ -5,25 +5,6 @@ import cloudinary from 'cloudinary-core';
 const cloudinaryCore = new cloudinary.Cloudinary({ cloud_name: process.env.CLOUDINARY_CLOUD_NAME });
 
 export default class SaveTheDate extends Component {
-  static renderSlideshow() {
-    return (
-      <Slideshow
-        showIndex={false}
-        showArrows={false}
-        autoplay
-        enableKeyboard={false}
-        useDotIndex
-        slideInterval={8000}
-        defaultIndex={0}
-        slides={[
-          cloudinaryCore.url('save-the-date/wedding1.png'),
-          cloudinaryCore.url('save-the-date/wedding2.png')
-        ]}
-        effect={'fade'}
-      />
-    );
-  }
-
   static renderDetails() {
     return (
       <div className="details">
@@ -34,11 +15,60 @@ export default class SaveTheDate extends Component {
     );
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowHeight: window.innerHeight,
+      windowWidth: window.innerWidth
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize());
+  }
+
+  handleResize() {
+    this.setState({
+      windowHeight: window.innerHeight,
+      windowWidth: window.innerWidth
+    });
+  }
+
+  renderSlideshow() {
+    const { windowHeight, windowWidth } = this.state;
+    const transformations = {
+      height: windowHeight,
+      width: windowWidth,
+      crop: 'fill',
+      gravity: 'faces'
+    };
+    return (
+      <Slideshow
+        showIndex={false}
+        showArrows={false}
+        autoplay
+        enableKeyboard={false}
+        useDotIndex
+        slideInterval={8000}
+        defaultIndex={0}
+        slides={[
+          cloudinaryCore.url('save-the-date/wedding1.png', transformations),
+          cloudinaryCore.url('save-the-date/wedding2.png', transformations)
+        ]}
+        effect={'fade'}
+      />
+    );
+  }
+
   render() {
     return (
       <>
         <h1>Save The Date</h1>
-        {SaveTheDate.renderSlideshow()}
+        {this.renderSlideshow()}
         {SaveTheDate.renderDetails()}
       </>
     );
