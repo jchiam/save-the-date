824ea30e707c9d0f017a7dfe393bd4a3ee4d0427
Jonathan Chiam <jczjchiam@gmail.com>
2018-11-24 17:00:22 +0800

    Overhaul design with slideshow

--- src/SaveTheDate.jsx
+++ src/SaveTheDate.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import Slideshow from 'react-slidez';
 
 export default class SaveTheDate extends Component {
   render() {
     return (
       <>
         <h1>Save The Date</h1>
-        <div className="image">
-          <img src={require('images/1.png')} />
-        </div>
-        <div className="details-container">
-          <div className="details">
-            <span>Rebecca + Jonathan</span>
-            <span>April 6, 2019</span>
-            <button>RSVP Here</button>
-          </div>
+        <Slideshow
+          showIndex={false}
+          showArrows={false}
+          autoplay
+          enableKeyboard={false}
+          useDotIndex
+          slideInterval={8000}
+          defaultIndex={0}
+          slides={[require('images/1.png'), require('images/2.jpg')]}
+          effect={'fade'}
+        />
+        <div className="details">
+          <span>Rebecca + Jonathan</span>
+          <span>April 6, 2019</span>
+          <button>RSVP Here</button>
         </div>
       </>
     );
